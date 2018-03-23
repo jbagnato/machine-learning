c07387175d9a6b7e6fc67661ca5becae15b95f5f
jbagnatoMacPro <nachobagnato@gmail.com>
2018-03-23 12:52:37 +0100

    Agrego archivos para comporbar versiones Python para data science

---
+++ versiones.py
@@ -0,0 +1,18 @@
+# scipy
+import scipy
+print('scipy: %s' % scipy.__version__)
+# numpy
+import numpy
+print('numpy: %s' % numpy.__version__)
+# matplotlib
+import matplotlib
+print('matplotlib: %s' % matplotlib.__version__)
+# pandas
+import pandas
+print('pandas: %s' % pandas.__version__)
+# statsmodels
+import statsmodels
+print('statsmodels: %s' % statsmodels.__version__)
+# scikit-learn
+import sklearn
+print('sklearn: %s' % sklearn.__version__)
---
+++ versiones_deep.py
@@ -0,0 +1,9 @@
+# theano
+import theano
+print('theano: %s' % theano.__version__)
+# tensorflow
+import tensorflow
+print('tensorflow: %s' % tensorflow.__version__)
+# keras
+import keras
+print('keras: %s' % keras.__version__)
