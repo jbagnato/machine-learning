3cc566f65bdf5b31db927412c00ce82275d2b5e3
jbagnatoMacPro <nachobagnato@gmail.com>
2021-01-10 12:13:02 +0100

    removed theano lines cause they are not in use

--- versiones_deep.py
+++ versiones_deep.py
@@ -1,9 +1,7 @@
-# theano
-import theano
-print('theano: %s' % theano.__version__)
 # tensorflow
 import tensorflow
 print('tensorflow: %s' % tensorflow.__version__)
 # keras
 import keras
 print('keras: %s' % keras.__version__)
+
